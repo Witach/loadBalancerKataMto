a10c99547d03340c8168cf9e4ee1e730129cd5c1
Dawid <215920@edu.p.lodz.pl>
2020-05-22 22:38:16 +0200

    parametrized tests for kata added

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,121 +1,170 @@
 package edu.iis.mto.serverloadbalancer;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static edu.iis.mto.serverloadbalancer.CurrentLoadPercentageMatcher.hasLoadPercentageOf;
 import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
+import static edu.iis.mto.serverloadbalancer.ServerShouldContainPredicate.getPredicateFromServerAndVmsIndexes;
 import static edu.iis.mto.serverloadbalancer.ServerVmsCountMatcher.hasVmsCountOf;
 import static edu.iis.mto.serverloadbalancer.VmBuilder.vm;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-import org.junit.Test;
 
 public class ServerLoadBalancerTest {
-	@Test
-	public void itCompiles() {
-		assertThat(true, equalTo(true));
-	}
+    @Test
+    public void itCompiles() {
+        assertThat(true, equalTo(true));
+    }
 
-	@Test
-	public void balancingAServer_noVms_serverStaysEmpty() {
-		Server theServer = a(server().withCapacity(1));
+    @Test
+    public void balancingAServer_noVms_serverStaysEmpty() {
+        Server theServer = a(server().withCapacity(1));
 
-		balance(aListOfServersWith(theServer), anEmptyListOfVms());
+        balance(aListOfServersWith(theServer), anEmptyListOfVms());
 
-		assertThat(theServer, hasLoadPercentageOf(0.0d));
-	}
+        assertThat(theServer, hasLoadPercentageOf(0.0d));
+    }
 
-	@Test
-	public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm() {
-		Server theServer = a(server().withCapacity(1));
-		Vm theVm = a(vm().ofSize(1));
-		balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+    @Test
+    public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm() {
+        Server theServer = a(server().withCapacity(1));
+        Vm theVm = a(vm().ofSize(1));
+        balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
 
-		assertThat(theServer, hasLoadPercentageOf(100.0d));
-		assertThat("the server should contain vm", theServer.contains(theVm));
-	}
+        assertThat(theServer, hasLoadPercentageOf(100.0d));
+        assertThat("the server should contain vm", theServer.contains(theVm));
+    }
 
-	@Test 
-	public void balancingOneServerWithTenSlotsCapacity_andOneSlotVm_fillTheServerWithTenPercent(){
-		Server theServer = a(server().withCapacity(10));
-		Vm theVm = a(vm().ofSize(1));
-		balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+    @Test
+    public void balancingOneServerWithTenSlotsCapacity_andOneSlotVm_fillTheServerWithTenPercent() {
+        Server theServer = a(server().withCapacity(10));
+        Vm theVm = a(vm().ofSize(1));
+        balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
 
-		assertThat(theServer, hasLoadPercentageOf(10.0d));
-		assertThat("the server should contain vm", theServer.contains(theVm));
-		
-	}
-	
-	@Test
-	public void balancingAServerWithEnoughRoom_getsFilledWithAllVms(){
-		Server theServer = a(server().withCapacity(100));
-		Vm theFirstVm = a(vm().ofSize(1));
-		Vm theSecondVm = a(vm().ofSize(1));
-		balance(aListOfServersWith(theServer), aListOfVmsWith(theFirstVm, theSecondVm));
+        assertThat(theServer, hasLoadPercentageOf(10.0d));
+        assertThat("the server should contain vm", theServer.contains(theVm));
 
-		assertThat(theServer, hasVmsCountOf(2));
-		assertThat("the server should contain vm", theServer.contains(theFirstVm));
-		assertThat("the server should contain vm", theServer.contains(theSecondVm));
+    }
 
-	}
+    @Test
+    public void balancingAServerWithEnoughRoom_getsFilledWithAllVms() {
+        Server theServer = a(server().withCapacity(100));
+        Vm theFirstVm = a(vm().ofSize(1));
+        Vm theSecondVm = a(vm().ofSize(1));
+        balance(aListOfServersWith(theServer), aListOfVmsWith(theFirstVm, theSecondVm));
 
-	@Test 
-	public void aVm_shouldBeBalanced_onLessLoadedServerFirst(){
-		Server lessLoadedServer = a(server().withCapacity(100).withCurrentLoadOf(45.0d));
-		Server moreLoadedServer = a(server().withCapacity(100).withCurrentLoadOf(50.0d));
-		Vm theVm = a(vm().ofSize(10));
-		
-		balance(aListOfServersWith(moreLoadedServer, lessLoadedServer), aListOfVmsWith(theVm));
+        assertThat(theServer, hasVmsCountOf(2));
+        assertThat("the server should contain vm", theServer.contains(theFirstVm));
+        assertThat("the server should contain vm", theServer.contains(theSecondVm));
 
-		assertThat("the less loaded server should contain vm", lessLoadedServer.contains(theVm));
+    }
 
-	}
+    @Test
+    public void aVm_shouldBeBalanced_onLessLoadedServerFirst() {
+        Server lessLoadedServer = a(server().withCapacity(100).withCurrentLoadOf(45.0d));
+        Server moreLoadedServer = a(server().withCapacity(100).withCurrentLoadOf(50.0d));
+        Vm theVm = a(vm().ofSize(10));
 
-	@Test 
-	public void balanceAServerWithNotEnoughRoom_shouldNotBeFilledWithAVm(){
-		Server theServer = a(server().withCapacity(10).withCurrentLoadOf(90.0d));
-		Vm theVm = a(vm().ofSize(2));
-		balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
-		
-		assertThat("the less loaded server should not contain vm", !theServer.contains(theVm));
-	}
+        balance(aListOfServersWith(moreLoadedServer, lessLoadedServer), aListOfVmsWith(theVm));
+
+        assertThat("the less loaded server should contain vm", lessLoadedServer.contains(theVm));
+
+    }
+
+    @Test
+    public void balanceAServerWithNotEnoughRoom_shouldNotBeFilledWithAVm() {
+        Server theServer = a(server().withCapacity(10).withCurrentLoadOf(90.0d));
+        Vm theVm = a(vm().ofSize(2));
+        balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+
+        assertThat("the less loaded server should not contain vm", !theServer.contains(theVm));
+    }
 
 
-	@Test 
-	public void balance_serversAndVms() {
-		Server server1 = a(server().withCapacity(4));
-        Server server2 = a(server().withCapacity(6));
-        
-        Vm vm1 = a(vm().ofSize(1));
-        Vm vm2 = a(vm().ofSize(4));
-        Vm vm3 = a(vm().ofSize(2));
-        
-        balance(aListOfServersWith(server1, server2), aListOfVmsWith(vm1, vm2, vm3));
-        
-        assertThat("The server 1 should contain the vm 1", server1.contains(vm1));
-        assertThat("The server 2 should contain the vm 2", server2.contains(vm2));
-        assertThat("The server 1 should contain the vm 3", server1.contains(vm3));
-        
-        assertThat(server1, hasLoadPercentageOf(75.0d));
-        assertThat(server2, hasLoadPercentageOf(66.66d));
-	}
+    @ParameterizedTest
+    @MethodSource("provideArgsForBalance_serversAndVms")
+    public void balance_serversAndVms(
+            Server[] servers, List<Double> percentages,
+            Vm[] vms, List<ServerShouldContainPredicate> serverShouldContainPredicates) {
 
-	private void balance(Server[] servers, Vm[] vms) {
-		new ServerLoadBalancer().balance(servers, vms);
-	}
+        String answerTemplate = "The server %d should contain the vm %d";
 
-	private Vm[] aListOfVmsWith(Vm... vms) {
-		return vms;
-	}
+        balance(aListOfServersWith(servers), aListOfVmsWith(vms));
 
-	private Vm[] anEmptyListOfVms() {
-		return new Vm[0];
-	}
+        serverShouldContainPredicates.forEach(predicate -> {
+            int serverIndex = predicate.getServerIndex();
+            predicate.getVmsIndexes().forEach(index -> {
+                String answer = String.format(answerTemplate, serverIndex, index);
+                assertThat(answer, servers[serverIndex].contains(vms[index]));
+            });
+        });
 
-	private Server[] aListOfServersWith(Server... servers) {
-		return servers;
-	}
+        for (int i = 0; i < servers.length ; i++) {
+            assertThat(servers[i],hasLoadPercentageOf(percentages.get(i)));
+        }
+    }
 
-	private <T> T a(Builder<T> builder) {
-		return builder.build();
-	}
+
+    private static Stream<Arguments> provideArgsForBalance_serversAndVms() {
+        Server[] servers = getListOfServersWithCapacity(4, 6);
+        Vm[] vms = getListOfVmsWithSize(1, 4, 2);
+        List<Double> percentages = List.of(
+                75.0d,
+                66.66d
+        );
+
+        List<ServerShouldContainPredicate> serverShouldContainPredicates = List.of(
+                getPredicateFromServerAndVmsIndexes(0, 0, 2),
+                getPredicateFromServerAndVmsIndexes(1, 1)
+        );
+
+
+        return Stream.of(
+                Arguments.of(servers, percentages, vms, serverShouldContainPredicates)
+        );
+    }
+
+
+    private static Server[] getListOfServersWithCapacity(int... capacities) {
+        return Arrays.stream(capacities)
+                .mapToObj(value -> a(server().withCapacity(value)))
+                .toArray(Server[]::new);
+    }
+
+    private static Vm[] getListOfVmsWithSize(int... sizes) {
+        return Arrays.stream(sizes)
+                .mapToObj(value -> a(vm().ofSize(value)))
+                .toArray(Vm[]::new);
+    }
+
+
+    private void balance(Server[] servers, Vm[] vms) {
+        new ServerLoadBalancer().balance(servers, vms);
+    }
+
+    private Vm[] aListOfVmsWith(Vm... vms) {
+        return vms;
+    }
+
+    private Vm[] anEmptyListOfVms() {
+        return new Vm[0];
+    }
+
+    private Server[] aListOfServersWith(Server... servers) {
+        return servers;
+    }
+
+    private static <T> T a(Builder<T> builder) {
+        return builder.build();
+    }
 }
---
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerShouldContainPredicate.java
@@ -0,0 +1,40 @@
+package edu.iis.mto.serverloadbalancer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ServerShouldContainPredicate {
+    private int serverIndex;
+    private List<Integer> vmsIndexes;
+
+    private ServerShouldContainPredicate(int serverIndex, List<Integer> vmsIndexes) {
+        this.serverIndex = serverIndex;
+        this.vmsIndexes = vmsIndexes;
+    }
+
+    public int getServerIndex() {
+        return serverIndex;
+    }
+
+    public List<Integer> getVmsIndexes() {
+        return vmsIndexes;
+    }
+
+    public String getPredicateWrongAnswer(){
+
+        String vmsIndexes = this.vmsIndexes.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(", "));
+
+        String indexPrompt = String.format("The server %d should contain the vms ", serverIndex);
+
+        return indexPrompt + vmsIndexes;
+    }
+
+    public static ServerShouldContainPredicate getPredicateFromServerAndVmsIndexes(int serverIndex, Integer ...vmsIndexes){
+        return new ServerShouldContainPredicate(serverIndex, Arrays.asList(vmsIndexes));
+    }
+
+
+}
